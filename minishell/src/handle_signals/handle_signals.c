dac2da2003f0a2cd9682d1b61c4dcc5dff41fe05
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2024-11-12 19:58:59 -0300

    handling sigint and sigquit

--- minishell/src/handle_signals/handle_signals.c
+++ minishell/src/handle_signals/handle_signals.c
@@ -1,6 +1,20 @@
-#include "../../includes/handle_signals.h"
+#include "../../includes/minishell.h"
+
+void	sigint_handler(int signal)
+{
+	if (signal == SIGINT)
+	{
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		ft_printf(STDIN_FILENO, "\n");
+		rl_redisplay();
+	}
+}
 
 void	handle_signals(void)
 {
 	printf("handle_signals\n");
+
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
 }
