5b274fa0eba61029957de1712418c3a8377c83bb
Murilo Dias <mdsouzadias@hotmail.com>
2024-11-13 15:19:10 +0000

    check mem_alloc of sheel->cmd

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mde-souz <mde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:09:36 by mde-souz          #+#    #+#             */
-/*   Updated: 2024/11/12 17:42:28 by mde-souz         ###   ########.fr       */
+/*   Updated: 2024/11/13 14:17:45 by mde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		shell->cmd = readline("minishell$ ");
-		if (shell->cmd)
-		
+		check_mem_alloc(shell, &(shell->mem_allocation.ptr_mem_list), \
+			shell->cmd, "Read line failed");
 		if (shell->cmd[0] != '\0')
 		{
 			add_history(shell->cmd);
