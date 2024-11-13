6992d426b7cabcb7af1ab33aacb93e74557cf21f
Murilo Dias <mdsouzadias@hotmail.com>
2024-11-13 15:18:10 +0000

    Replace error code to with MACRO

--- minishell/src/utils/handle_error.c
+++ minishell/src/utils/handle_error.c
@@ -17,18 +17,18 @@ void	free_all(t_shell *shell)
 
 void	msg_error(t_error_codes error_code, char *error_msg)
 {
-	if (error_code == 1)
+	if (error_code == CMD_NOT_FOUND)
 		ft_printf(STDERR_FILENO, "%s: command not found\n", error_msg);
-	else if (error_code == 2)
+	else if (error_code == NO_FILE_DIRECTORY)
 		ft_printf(STDERR_FILENO, "minishell: %s: No such file or directory\n",
 			error_msg);
-	else if (error_code == 3)
+	else if (error_code == PERMISSION_DENIED)
 		ft_printf(STDERR_FILENO, "minishell: %s: Permission denied\n", error_msg);
-	else if (error_code == 4)
+	else if (error_code == AMBIGUOS_REDIRECT)
 		ft_printf(STDERR_FILENO, "minishell: ambiguous redirect\n", error_msg);
-	else if (error_code == 5)
+	else if (error_code == IS_DIRECTORY)
 		ft_printf(STDERR_FILENO, "minishell: %s: Is a directory\n", error_msg);
-	else if (error_code == 6)
+	else if (error_code == IS_NOT_DIRECTORY)
 		ft_printf(STDERR_FILENO, "minishell: %s: Not a directory\n", error_msg);
 	else
 		ft_printf(STDERR_FILENO, "%s\n", error_msg);
