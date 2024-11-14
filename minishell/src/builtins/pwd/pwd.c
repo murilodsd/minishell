3382b999d9c80c7a2313e2107d552927ee09e961
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2024-11-14 09:36:03 -0300

    pwd done and doing cd

--- minishell/src/builtins/pwd/pwd.c
+++ minishell/src/builtins/pwd/pwd.c
@@ -1,6 +1,23 @@
 #include "../../../includes/minishell.h"
 
+char	*safe_getcwd(char *buf, size_t size, t_shell *shell)
+{
+	char	*cwd;
+
+	cwd = getcwd(buf, size);
+	check_mem_alloc(shell, &(shell->mem_allocation.ptr_mem_list), \
+		cwd, "Getcwd failed");
+	//REVIEW - AQUI DÁ EXIT OU NÃO SE FALHAR?
+	return (cwd);
+}
+
 void	pwd_builtin(void)
 {
-	printf("\t\tpwd\n");
+	char	*pwd;
+
+	pwd = safe_getcwd(NULL, 0, NULL);
+	ft_printf(1, "%s\n", pwd);
+	free(pwd);
+	//REVIEW - PRECISA SETAR A SAÍDA AQUI?
+	//shell->exit_status = EXIT_SUCCESS;
 }
