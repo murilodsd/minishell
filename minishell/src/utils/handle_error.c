71e6af26893c4a29be8bd650a42b316e6a8491e8
dramos-j <danielarjordao@hotmail.com>
2024-11-15 15:28:23 +0000

    check_syntax

--- minishell/src/utils/handle_error.c
+++ minishell/src/utils/handle_error.c
@@ -50,6 +50,26 @@ void	msg_error(t_error_codes error_code, char *error_msg, ...)
 	va_end(va_args);
 	}
 	errno = 0;
+void	msg_error(t_error_codes error_code, char *error_msg)
+{
+	if (error_code == CMD_NOT_FOUND)
+		ft_printf(STDERR_FILENO, "%s: command not found\n", error_msg);
+	else if (error_code == NO_FILE_DIRECTORY)
+		ft_printf(STDERR_FILENO, "minishell: %s: No such file or directory\n",
+			error_msg);
+	else if (error_code == PERMISSION_DENIED)
+		ft_printf(STDERR_FILENO, "minishell: %s: Permission denied\n", error_msg);
+	else if (error_code == AMBIGUOS_REDIRECT)
+		ft_printf(STDERR_FILENO, "minishell: ambiguous redirect\n", error_msg);
+	else if (error_code == IS_DIRECTORY)
+		ft_printf(STDERR_FILENO, "minishell: %s: Is a directory\n", error_msg);
+	else if (error_code == IS_NOT_DIRECTORY)
+		ft_printf(STDERR_FILENO, "minishell: %s: Not a directory\n", error_msg);
+	else if (error_code == SYNTAX_ERROR)
+		ft_printf(STDERR_FILENO, "minishell: syntax error near unexpected token `%s'\n",
+			error_msg);
+	else
+		ft_printf(STDERR_FILENO, "%s\n", error_msg);
 }
 
 void	free_exit_error(t_shell *shell, t_error_codes error_code, char *error_msg)
