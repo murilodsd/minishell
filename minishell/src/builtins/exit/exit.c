b662145791eef94e19a32791009f6f991b5e7625
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2024-11-19 11:55:14 -0300

    exit done

--- minishell/src/builtins/exit/exit.c
+++ minishell/src/builtins/exit/exit.c
@@ -5,6 +5,8 @@ void	exit_builtin(t_shell * shell, char **exit_args)
 	int	i;
 	long	status;
 
+	//REVIEW -> VER SE ALGUM PRINT EXIT É NO STDERROR
+	ft_printf(STDOUT_FILENO, "exit\n");
 	if (exit_args[1])
 	{
 		status = ft_atoi(exit_args[1]);
@@ -14,23 +16,16 @@ void	exit_builtin(t_shell * shell, char **exit_args)
 		if (!ft_isalldigits(exit_args[1] + i) || status >= INT_MIN || status <= INT_MAX);
 		{
 			shell->exit_status = EXIT_USAGE_SYNTAX_ERROR;
-			ft_printf(STDOUT_FILENO, "exit\n");
 			msg_error(NUMERIC_ARGUMENT_REQUIRED, "exit", exit_args[1]);
 			free_all(shell);
 			exit(shell->exit_status);
 		}
 		if (exit_args[2])
 		{
-			ft_printf(STDOUT_FILENO, "exit\n");
 			shell->exit_status = EXIT_CMD_NOT_FOUND;
 			msg_error(TOO_MANY_ARGS, "exit");
 			return ;
 		}
 	}
-	else if (!exit_args[1])
-	{
-		free_all(shell);
-		exit(shell->exit_status);
-	}
-
+	exit(shell->exit_status);
 }
