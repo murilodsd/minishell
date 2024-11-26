5227bc7cb21acd1ad65bfd084cc0f72071c83875
Murilo Dias <mdsouzadias@hotmail.com>
2024-11-26 19:51:37 +0000

    sinais tratados

--- minishell/src/handle_signals/handle_signals.c
+++ minishell/src/handle_signals/handle_signals.c
@@ -13,7 +13,24 @@ void	sigint_handler(int signal)
 
 void	handle_signals(void)
 {
-	printf("handle_signals\n");
 	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	reset_sig_int_and_quit(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
+
+void	reset_sig_int_ignore_sig_quit(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	ignore_sig_int_and_quit(void)
+{
+	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 }
