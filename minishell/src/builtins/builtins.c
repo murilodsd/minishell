fef8086fe8fc16cf45d7823755262d727100d583
Murilo Dias <mdsouzadias@hotmail.com>
2024-11-26 19:52:18 +0000

    if da exeucao dos builtins

--- minishell/src/builtins/builtins.c
+++ minishell/src/builtins/builtins.c
@@ -1,13 +1,22 @@
 #include "../../includes/minishell.h"
 
-void	builtins(void)
+void	builtins(t_shell *shell, char **args)
 {
-	printf("Builtins:\n");
-/* 	cd_builtin();
-	echo_builtin();
-	env_builtin();
-	exit_builtin();
-	export_builtin();
-	pwd_builtin(shell);
-	unset_builtin(); */
+	if (!ft_strcmp(args, "pwd"))
+		pwd_builtin(shell);
+	if (!ft_strcmp(args, "echo"))
+		echo_builtin(shell, args);
+	if (!ft_strcmp(args, "cd"))
+		cd_builtin(shell, args);
+	if (!ft_strcmp(args, "export"))
+		export_builtin(shell, args);
+	if (!ft_strcmp(args, "unset"))
+		unset_builtin(shell, args);
+	if (!ft_strcmp(args, "env"))
+		env_builtin(shell);
+	if (!ft_strcmp(args, "exit"))
+		exit_builtin(shell, args);
+	else
+		//exec
+		ft_printf(1, "exec");
 }
