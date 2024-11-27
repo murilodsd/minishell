d1b046ddbb5eff2aa1a1a15c717da559e180ddf6
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2024-11-26 21:23:54 -0300

    Refactor last_redir function in binary_tree_utils.c

--- minishell/src/binary_tree/binary_tree_utils.c
+++ minishell/src/binary_tree/binary_tree_utils.c
@@ -1,16 +1,5 @@
 #include "../../includes/minishell.h"
 
-bool	last_redir(t_token *token)
-{
-	if (token->type != REDIR_IN && token->type != REDIR_OUT
-		&& token->type != REDIR_APPEND && token->type != HEREDOC)
-		return (false);
-	token = get_next_redir(token);
-	if (!token)
-		return (true);
-	return (false);
-}
-
 bool	is_redir_token(t_token *token)
 {
 	if (!token)
