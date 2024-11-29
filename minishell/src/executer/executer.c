fcc0a8994977062878b53ab40c1155adb39b7743
Murilo Dias <mdsouzadias@hotmail.com>
2024-11-29 18:41:37 +0000

    fazendo execucao

--- minishell/src/executer/executer.c
+++ minishell/src/executer/executer.c
@@ -1,6 +1,41 @@
 #include "../../includes/minishell.h"
 
-void	executer(void)
+void	run_exec(t_shell *shell, t_exec *exec)
 {
-	printf("Executer\n");
+	if (!exec || !exec->args[0])
+		return ;
+	//REVIEW -> caso de o comando ser "" ou $abc
+	//if (!exec->args[0][0])
+	//	shell_error(shell, "", 1, true);
+	if (!ft_strcmp(exec->args[0], "echo"))
+		echo(exec->args, shell);
+	else if (!ft_strcmp(exec->args[0], "export"))
+		export(exec->args, shell);
+	else if (!ft_strcmp(exec->args[0], "unset"))
+		unset(exec->args, shell);
+	else if (!ft_strcmp(exec->args[0], "pwd"))
+		pwd(shell);
+	else if (!ft_strcmp(exec->args[0], "cd"))
+		cd(exec->args, shell);
+	else if (!ft_strcmp(exec->args[0], "exit"))
+		exit_cmd(exec->args, shell);
+	//REVIEW - Caso de nao ser nenhum comando valido
+	else
+		run_execv(shell, exec);
+	free_and_exit(shell);
+}
+
+void	executer(t_shell * shell, void *root)
+{
+	t_node_type	node_type;
+
+	if (!root)
+		return ;
+	node_type = *(t_node_type*)root;
+	if (node_type == EXEC_NODE)
+		run_exec(shell, (t_exec *)root);
+	else if (is_redir_node_type(node_type))
+		run_redir(shell, (t_redir *)root);
+	else if (node_type == PIPE_NODE)
+		run_pipe(shell, (t_pipe *)root);
 }
