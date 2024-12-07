b40bfcc9c32065195eede3aaff09f6f75b8155de
dramos-j <danielarjordao@hotmail.com>
2024-12-07 19:39:09 +0000

    feat: Enhance signal handling and add get_child_status function

--- minishell/src/handle_signals/handle_signals_utils.c
+++ minishell/src/handle_signals/handle_signals_utils.c
@@ -6,7 +6,7 @@
 /*   By: dramos-j <dramos-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 15:52:23 by dramos-j          #+#    #+#             */
-/*   Updated: 2024/12/07 15:52:24 by dramos-j         ###   ########.fr       */
+/*   Updated: 2024/12/07 19:31:57 by dramos-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,11 @@ void	handle_ctrl_c(t_shell *shell)
 	shell->exit_status = 130;
 	clear_token_list(shell);
 	clear_heredoc_list(shell);
+	g_signal = NONE;
 }
 
 void	handle_ctrl_c_hd(t_shell *shell)
 {
-	open("/dev/tty", O_RDONLY);
-	shell->exit_status = 130;
 	free_restart(shell);
 	g_signal = NONE;
 }
@@ -38,8 +37,8 @@ void	handle_hd_sigint(int signal)
 {
 	if (signal == SIGINT)
 	{
-		ft_printf(STDIN_FILENO, "\n");
 		g_signal = CTRL_C_HD;
+		ft_printf(STDIN_FILENO, "\n");
 		close(STDIN_FILENO);
 	}
 }
--- minishell/src/heredoc/heredoc.c
+++ minishell/src/heredoc/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: dramos-j <dramos-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 17:30:21 by dramos-j          #+#    #+#             */
-/*   Updated: 2024/12/07 15:49:39 by dramos-j         ###   ########.fr       */
+/*   Updated: 2024/12/07 19:38:09 by dramos-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,10 @@ void	fill_fd_heredoc(t_heredoc *tmp_hd, t_shell *shell)
 {
 	char	*line;
 
-	reset_sig_int_ignore_sig_quit();
 	while (1)
 	{
 		line = readline("> ");
-		if (!line)
+		if (!line || g_signal == CTRL_C_HD)
 		{
 			if (g_signal != CTRL_C_HD)
 				printf("minishell: warning: here-document delimited" \
@@ -62,13 +61,22 @@ void	fill_fd_heredoc(t_heredoc *tmp_hd, t_shell *shell)
 		write(tmp_hd->fd_heredoc, "\n", 1);
 		free(line);
 	}
-	handle_signals();
+}
+
+void	handle_heredoc_child(t_heredoc *tmp_hd, t_shell *shell)
+{
+	reset_sig_int_ignore_sig_quit();
+	fill_fd_heredoc(tmp_hd, shell);
+	close(tmp_hd->fd_heredoc);
+	exit(0);
 }
 
 void	heredoc(t_shell *shell)
 {
 	t_heredoc	*tmp_hd;
 	char		*i;
+	int			status;
+	pid_t		pid;
 
 	save_heredoc_info(shell);
 	tmp_hd = shell->heredoc;
@@ -79,12 +87,14 @@ void	heredoc(t_shell *shell)
 		free(i);
 		tmp_hd->fd_heredoc = \
 			open(tmp_hd->fd_heredoc_path, O_CREAT | O_RDWR | O_TRUNC, 0644);
-		fill_fd_heredoc(tmp_hd, shell);
-		close(tmp_hd->fd_heredoc);
-		if (g_signal == CTRL_C_HD)
+		pid = safe_fork(shell, NULL);
+		if (pid == 0)
+			handle_heredoc_child(tmp_hd, shell);
+		waitpid(pid, &status, 0);
+		if (get_child_status(status) == 130)
 		{
 			handle_ctrl_c_hd(shell);
-			return ;
+			break ;
 		}
 		tmp_hd = tmp_hd->next;
 	}
